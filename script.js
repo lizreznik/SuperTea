73c1abde93d9f9d594660c7a04f4430adbcab586
lizreznik <525ereznik@frhsd.com>
2024-05-08 10:43:34 -0400

    added imgs + updated text

--- script.js
+++ script.js
@@ -1,19 +1,192 @@
 const teamMembers = [
     {
-        name: "Lionel Messi",
-        position: "Forward",
-        skills: ["Dribbling", "Shooting", "Passing"],
-        strengths: "Ball control",
-        weaknesses: "Heading ability",
-        biography: "Lionel Messi is considered one of the greatest players of all time etc etc",
+        name: "Drake",
+        pic: "drake.jpg",
+        genre: "...",
+        album: "...",
+        song: "...",
+        favorite: ["...", "...", "..."],
+        biography: "...",
     },
     {
-        name: "Lionel Messi Jr",
-        position: "Midfielder",
-        skills: ["Dribbling", "Shooting", "Passing"],
-        strengths: "Ball control",
-        weaknesses: "Heading ability",
-        biography: "Lionel Messi Jr is considered one of the greatest players of all time etc etc",
+        name: "Taylor Swift",
+        pic: "taylorswift.jpg",
+        genre: "...",
+        album: "...",
+        song: "...",
+        favorite: ["...", "...", "..."],
+        biography: "...",
+    },
+    {
+        name: "Bryson Tiller", 
+        pic: "brysontiller.jpg",
+        genre: "...",
+        album: "...",
+        song: "...",
+        favorite: ["...", "...", "..."],
+        biography: "...",
+    },
+    {
+        name: "The Weeknd", 
+        pic: "weeknd.jpg",
+        genre: "...",
+        album: "...",
+        song: "...",
+        favorite: ["...", "...", "..."],
+        biography: "...",
+    },
+    {
+        name: "Miguel", 
+        pic: "miguel.jpg",
+        genre: "...",
+        album: "...",
+        song: "...",
+        favorite: ["...", "...", "..."],
+        biography: "...",
+    },
+    {
+        name: "Brent Faiyaz", 
+        pic: "brent.jpg",
+        genre: "...",
+        album: "...",
+        song: "...",
+        favorite: ["...", "...", "..."],
+        biography: "...",
+    },
+    {
+        name: "Frank Ocean", 
+        pic: "frank.jpg",
+        genre: "...",
+        album: "...",
+        song: "...",
+        favorite: ["...", "...", "..."],
+        biography: "...",
+    },
+    {
+        name: "Kendrick Lamar", 
+        pic: "kendrick.jpg",
+        genre: "...",
+        album: "...",
+        song: "...",
+        favorite: ["...", "...", "..."],
+        biography: "...",
+    },
+    {
+        name: "J. Cole", 
+        pic: "jcole.jpg",
+        genre: "...",
+        album: "...",
+        song: "...",
+        favorite: ["...", "...", "..."],
+        biography: "...",
+    },
+    {
+        name: "Tory Lanez", 
+        pic: "tory.jpg",
+        genre: "...",
+        album: "...",
+        song: "...",
+        favorite: ["...", "...", "..."],
+        biography: "...",
+    },
+    {
+        name: "Steve Lacey", 
+        pic: "steve.jpg",
+        genre: "...",
+        album: "...",
+        song: "...",
+        favorite: ["...", "...", "..."],
+        biography: "...",
+    },
+    {
+        name: "The Neighbourhood", 
+        pic: "nieghbourhood.jpg",
+        genre: "...",
+        album: "...",
+        song: "...",
+        favorite: ["...", "...", "..."],
+        biography: "...",
+    },
+    {
+        name: "The Interrupters", 
+        pic: "interrupters.jpg",
+        genre: "...",
+        album: "...",
+        song: "...",
+        favorite: ["...", "...", "..."],
+        biography: "...",
+    },
+    {
+        name: "Paramore", 
+        pic: "paramore.jpg",
+        genre: "...",
+        album: "...",
+        song: "...",
+        favorite: ["...", "...", "..."],
+        biography: "...",
+    },
+    {
+        name: "Daniel Ceaser", 
+        pic: "danceaser.jpg",
+        genre: "...",
+        album: "...",
+        song: "...",
+        favorite: ["...", "...", "..."],
+        biography: "...",
+    },
+    {
+        name: "Travis Scott", 
+        pic: "travis.jpg",
+        genre: "...",
+        album: "...",
+        song: "...",
+        favorite: ["...", "...", "..."],
+        biography: "...",
+    },
+    {
+        name: "TV Girl", 
+        pic: "tvgirl.jpg",
+        genre: "...",
+        album: "...",
+        song: "...",
+        favorite: ["...", "...", "..."],
+        biography: "...",
+    },
+    {
+        name: "Tyler, The Creator", 
+        pic: "tylerthecreator.jpg",
+        genre: "...",
+        album: "...",
+        song: "...",
+        favorite: ["...", "...", "..."],
+        biography: "...",
+    },
+    {
+        name: "Lana Del Ray", 
+        pic: "lana.jpg",
+        genre: "...",
+        album: "...",
+        song: "...",
+        favorite: ["...", "...", "..."],
+        biography: "...",
+    },
+    {
+        name: "Summer Walker", 
+        pic: "summer.jpg",
+        genre: "...",
+        album: "...",
+        song: "...",
+        favorite: ["...", "...", "..."],
+        biography: "...",
+    },
+    {
+        name: "Ariana Grande", 
+        pic: "ariana.jpg",
+        genre: "...",
+        album: "...",
+        song: "...",
+        favorite: ["...", "...", "..."],
+        biography: "...",
     },
 ];
 
@@ -27,18 +200,31 @@ function generateTeamCards() {
         // styling card based on position: 
         let backgroundColor
 
+        switch (member.genre.toLowerCase) {
+            case 'forward':
+                backgroundColor = '#ffc107'
+                break
+        }
+
+        card.style.backgroundColor = backgroundColor
+
         //create a list of skills with <li> tags //
-        const skillsList = member.skills.map(skill => `<li> ${skill} </li> `).join('')
+        const favoriteList = member.favorite.map(favorite => `<li> ${favorite} </li> `).join('')
 
         card.innerHTML = `
     <div class = "card"> 
-        <div class = card-header"> ${member.name} </div> 
+        <div class = "card-header"> ${member.name} </div> 
         <div class = "card-body"> 
-            <p><strong>Position:</strong> ${member.position} </p> 
-            <p><strong>Skills:</strong> <ul> ${skillsList} </ul> </p>
-            <p><strong>Strengths:</strong> ${member.strengths} </p>
-            <p><strong>Weaknesses:</strong> ${member.weaknesses} </p>
+            <div class = "card-img">
+                <img class="img-fluid" src= ${member.pic}>
+            </div>
+            <div class = "card-text"> 
+            <p><strong>Music Genre:</strong> ${member.genre} </p> 
+            <p><strong>Best Album:</strong> ${member.album} </p>
+            <p><strong>Most Popular Song:</strong> ${member.song} </p>
+             <p><strong>Personal Favorites:</strong> <ul> ${favoriteList} </ul> </p>
             <p><strong>Biography:</strong> ${member.biography} </p>
+            </div> 
         </div> 
     </div> 
     `
